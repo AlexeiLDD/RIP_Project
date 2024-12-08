9b30dc5c5bff73554653859850a76a3b190586e1
AlexeiLDD <doroga218@gmail.com>
2024-12-09 00:52:18 +0300

    added findNotesByUser and findNoteByIdAndUser to NoteRepository and NoteService

--- src/main/java/com/rip/RIP_Project/repository/NoteRepository.java
+++ src/main/java/com/rip/RIP_Project/repository/NoteRepository.java
@@ -1,7 +1,14 @@
 package com.rip.RIP_Project.repository;
 
+import com.rip.RIP_Project.entity.CustomUser;
 import com.rip.RIP_Project.entity.Note;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findNotesByUser(CustomUser user);
+    Optional<Note> findNoteByIdAndUser(Long id, CustomUser user);
 }
--- src/main/java/com/rip/RIP_Project/service/NoteService.java
+++ src/main/java/com/rip/RIP_Project/service/NoteService.java
@@ -1,41 +1,40 @@
 package com.rip.RIP_Project.service;
 
+import com.rip.RIP_Project.entity.CustomUser;
 import com.rip.RIP_Project.entity.Note;
 import com.rip.RIP_Project.repository.NoteRepository;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 
 @Service
 public class NoteService {
     private final NoteRepository noteRepository;
 
-    public NoteService(NoteRepository noteRepository) {
-        this.noteRepository = noteRepository;
-    }
+    public NoteService(NoteRepository noteRepository) { this.noteRepository = noteRepository; }
 
     // Получить все заметки
-    public List<Note> getAllNotes() {
-        return noteRepository.findAll();
-    }
+    public List<Note> getAllNotes(CustomUser user) { return noteRepository.findNotesByUser(user); }
 
     // Получить заметку по ID
-    public Note getNoteById(Long id) {
-        return noteRepository.findById(id).orElse(null);
+    public Note getNoteById(Long id) { return noteRepository.findById(id).orElse(null); }
+
+    // Получить заметку по ID и пользователю
+    public Note getNoteByIdAndUser(Long id, CustomUser user) {
+        return noteRepository.findNoteByIdAndUser(id, user)
+                .orElseThrow(() -> new RuntimeException("Note not found"));
     }
 
     // Создать новую заметку
-    public Note createNote(Note note) {
-        return noteRepository.save(note);
-    }
+    public Note createNote(Note note) { return noteRepository.save(note); }
 
     // Удалить заметку по ID
-    public void deleteNoteById(Long id) {
-        noteRepository.deleteById(id);
-    }
+    public void deleteNoteById(Long id) { noteRepository.deleteById(id); }
 
-    public Note updateNote(Long id, Note updatedNote) {
-        Note existingNote = noteRepository.findById(id)
+    public Note updateNote(Long id, CustomUser user, Note updatedNote) {
+        Note existingNote = noteRepository.findNoteByIdAndUser(id, user)
                 .orElseThrow(() -> new RuntimeException("Note not found"));
         existingNote.setTitle(updatedNote.getTitle());
         existingNote.setContent(updatedNote.getContent());
